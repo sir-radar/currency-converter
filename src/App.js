f67518ae7ac7203a2d0402e8ec7cb0bdad9ae016
MAC <nwokikesamson@gmail.com>
2020-02-12 00:15:10 +0100

    ui set up and design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow'
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 
 function App() {
+  const [currencyOptions, setCurrencyOptions]
+  useEffect(()=>{
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Convert</h1>
+    <CurrencyRow/>
+    <div className="equals">=</div>
+    <CurrencyRow/>
+    </>
   );
 }
 
---
+++ src/CurrencyRow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CurrencyRow = () => {
+  return (
+    <div>
+      <input className="input" type="number"/>
+      <select name="" id="">
+        <option value="Hi">Hi</option>
+      </select>
+    </div>
+  )
+}
+
+export default CurrencyRow
